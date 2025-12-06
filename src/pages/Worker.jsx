5a9c8254dd205a370862a63d5b9bd0d01588c8be
lilianzghibarta-collab <lilian.zghibarta@gmail.com>
2025-12-06 17:13:48 +0000

    Add Worker component for dashboard demo

---
+++ src/pages/Worker.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+export default function Worker(){
+  return (
+    <div className="container">
+      <h1>Worker Dashboard (demo)</h1>
+      <p>Use accept-invite link to register and then test Start/Finish actions.</p>
+    </div>
+  );
+}
